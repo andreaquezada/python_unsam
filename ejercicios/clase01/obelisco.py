92dcd32f9e177b3c53c39834a6c4db8f16f05501
Andrea Quezada <andreagtzq@gmail.com>
2021-03-12 19:25:53 -0600

    He creado una carpeta de ejercicios y dentro el primer ejercicio de la clase 01

---
+++ ejercicios/clase01/obelisco.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 12 18:47:10 2021
+
+@author: andrea
+"""
+
+grosor_billete = 0.11 * 0.001 # grosor de un billete en metros
+altura_obelisco = 67.5         # altura en metros
+num_billetes = 1
+dia = 1
+
+
