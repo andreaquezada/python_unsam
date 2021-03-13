5801b0ab991b29a0b74a0782308854dc374018ea
Andrea Quezada <andreagtzq@gmail.com>
2021-03-12 23:38:32 -0600

    He añadido el programa esfera.py

---
+++ ejercicios/clase01/esfera.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(username)
+Twitter: @andreaquezadaa
+"""
+import math
+
+radio = float(input('Tienes una esfera de radio:'))
+print('El vollumen es', round(4/3*math.pi*math.pow(radio, 3), 4))
